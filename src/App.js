108cfa270f7115cb37ecc38280cdffcb5ee62f90
Emilio Zanoni <emiliozanoni@hotmail.com>
2022-07-10 19:58:38 -0300

    change react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react"
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.js"
 
 function App(){
     return(
         <>
-            <BrowserRouter basename={process.env.PUBLIC_URL}>
+            {/* <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <Routes>
                     <Route path="/" element={<Home />} />
                 </Routes>
-            </BrowserRouter>
+            </BrowserRouter> */}
+            <Home />
         </>
     )
 }
