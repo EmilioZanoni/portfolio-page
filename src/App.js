38d6b63e44ef0baf5f57167bbf03e61a8bafeb93
Emilio Zanoni <emiliozanoni@hotmail.com>
2022-07-10 19:47:42 -0300

    changes to work

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/Home.js"
 function App(){
     return(
         <>
-            <BrowserRouter>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <Routes>
                     <Route path="/" element={<Home />} />
                 </Routes>
